2f9291e650f5ef9c3560b363124aae7ada1c6ebb
Joshua Duffney <duffney@outlook.com>
2016-02-12 14:34:33 -0600

    Working version

--- Utilities/Publish-DSCResourcePull.ps1
+++ Utilities/Publish-DSCResourcePull.ps1
@@ -9,10 +9,10 @@ foreach ($ModuleName in $Module){
     $ModuleVersion = (Get-Module $ModuleName -ListAvailable).Version
     
     Compress-Archive -Update `
-    -Path "$Env:PROGRAMFILES\WindowsPowerShell\Modules\$ModuleName" `
-    -DestinationPath "$Env:PROGRAMFILES\WindowsPowerShell\DscService\Modules\$($ModuleName)_$($ModuleVersion)"
+    -Path "$Env:PROGRAMFILES\WindowsPowerShell\Modules\$ModuleName\$ModuleVersion\*" `
+    -DestinationPath "$Env:PROGRAMFILES\WindowsPowerShell\DscService\Modules\$($ModuleName)_$($ModuleVersion).zip"
     
-    New-DscChecksum "$Env:PROGRAMFILES\WindowsPowerShell\DscService\Modules\$($ModuleName)_$($ModuleVersion)"
+    New-DscChecksum "$Env:PROGRAMFILES\WindowsPowerShell\DscService\Modules\$($ModuleName)_$($ModuleVersion).zip"
 }
 
 }
