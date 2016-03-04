a6df270ba288d5aff42452b10c095601e58817e1
Joshua Duffney <duffney@outlook.com>
2016-03-04 11:18:59 -0600

    Enabled the ability to execute remotely
    Updated the code base to include invoke-command which will stage the resources on a remote pull server or local which ever is specified.

--- Utilities/Publish-DSCResourcePull.ps1
+++ Utilities/Publish-DSCResourcePull.ps1
@@ -1,18 +1,44 @@
 function Publish-DSCResourcePull {
 Param(
     [string[]]
-    $Module
+    $Module,
+    $ComputerName = $env:COMPUTERNAME,
+    $Credential
 )
+
+if ($PSBoundParameters.ContainsKey('Credential')){
+    $SecurePassword = Read-Host -Prompt "Enter Password" -AsSecureString
+    $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $Credential,$SecurePassword
+}
+
+$Parms = @{
+    'ComputerName' = $ComputerName
+}
+
+if ($Credential) {$Parms.Add('Credential',$Credential)}
+
+$session = New-PSSession @Parms
+
 
 foreach ($ModuleName in $Module){
     
+    $From = (Get-ChildItem -Path $Env:PROGRAMFILES\WindowsPowerShell\Modules | where Name -eq $ModuleName).FullName
+    $To =  "$Env:PROGRAMFILES\WindowsPowerShell\Modules"
     $ModuleVersion = (Get-Module $ModuleName -ListAvailable).Version
     
-    Compress-Archive -Update `
+    Write-Verbose -Message "Copying $ModuleName to $ComputerName..."
+
+    Copy-Item -Path $From -Recurse -Destination $To -ToSession $session
+
+    Write-Verbose -Message "Creating $ModuleName archive..."
+
+    Invoke-Command -Session $session -ScriptBlock {Param($ModuleName,$ModuleVersion)Compress-Archive -Update `
     -Path "$Env:PROGRAMFILES\WindowsPowerShell\Modules\$ModuleName\$ModuleVersion\*" `
-    -DestinationPath "$Env:PROGRAMFILES\WindowsPowerShell\DscService\Modules\$($ModuleName)_$($ModuleVersion).zip"
-    
-    New-DscChecksum "$Env:PROGRAMFILES\WindowsPowerShell\DscService\Modules\$($ModuleName)_$($ModuleVersion).zip"
+    -DestinationPath "$Env:PROGRAMFILES\WindowsPowerShell\DscService\Modules\$($ModuleName)_$($ModuleVersion).zip"} `
+    -ArgumentList $ModuleName,$ModuleVersion
+
+    Write-Verbose -Message "Creating $ModuleName checksum..."
+    Invoke-Command -Session $session -ScriptBlock {New-DscChecksum "$Env:PROGRAMFILES\WindowsPowerShell\DscService\Modules\$($ModuleName)_$($ModuleVersion).zip"}
 }
 
 }
