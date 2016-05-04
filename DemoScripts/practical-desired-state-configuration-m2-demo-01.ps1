29af497c15005ce08b2c91d3cff767f74a3589da
Joshua Duffney <duffney@outlook.com>
2016-05-03 23:15:18 -0500

    updated demo 1 script

--- DemoScripts/practical-desired-state-configuration-m2-demo-01.ps1
+++ DemoScripts/practical-desired-state-configuration-m2-demo-01.ps1
@@ -1,10 +1,20 @@
-#Discovering DSC Resource Modules
+# Discovering DSC Resource Modules
 Find-Module -Includes DscResource
 Find-Module -Tag DSC
 Find-Module -Name *Networking*  -Includes DscResource
 
-#Discovering DSC Resources
+# Finding Different Module Versions
+Find-Module xnetworking -AllVersions
+Find-Module xnetworking -MaximumVersion 2.5.0.0
+Find-Module xnetworking -MinimumVersion 2.7.0.0
+Find-Module xnetworking -RequiredVersion 2.6.0.0
+
+# Discovering DSC Resources
 Find-DscResource
 Find-DscResource -moduleName xNetworking
-Find-DscResource | Where-Object Name -like *IP*
+Find-DscResource -moduleName xNetworking -RequiredVersion 2.4.0.0
 Find-DscResource -Name xIPAddress
+
+# if ($Possible){ Filter left} 
+Find-DscResource -Filter "network"
+Find-DscResource | Where-Object Name -like *IP*
