15b4ae50f918bc1de8f9f7a9e06418bd75387e6b
Joshua Duffney <duffney@outlook.com>
2016-05-03 21:18:03 -0500

    renamed demo scripts

--- DemoScripts/practical-desired-state-configuration-m2-00.ps1
+++ DemoScripts/practical-desired-state-configuration-m2-00.ps1
@@ -1,28 +1,29 @@
-Configuration RenameComputer {
-    param(
-        [string]$NodeName,
-        [string]$NewName
-    )
+#Discovering DSC Resource Modules
+Find-Module -Includes DscResource
+Find-Module -Tag DSC
+Find-Module -Name *Networking*  -Includes DscResource
 
-    Import-DscResource -ModuleName xComputerManagement 
-            
-    Node $AllNodes.NodeName {         
-    
-    xComputer RenameComputer {
-        Name = $Node.NewName
-    }
-    
-    } #end node block
-    
-} #end configuration
+#Discovering DSC Resources
+Find-DscResource
+Find-DscResource -moduleName xNetworking
+Find-DscResource | Where-Object Name -like *IP*
+Find-DscResource -Name xIPAddress
 
-$ConfigData = @{             
-    AllNodes = @(             
-        @{             
-            Nodename = 'WIN-6J22PI2U9RJ'
-            NewName = 'PS-S01'            
-        }                   
-    )             
-}
 
-RenameComputer -ConfigurationData $ConfigData -OutputPath C:\dsc\push
+#Installing DSC Resource Modules
+Install-Module xnet*
+Install-Module -Name xNetworking
+Install-Module -Name xNetworking -Force
+Install-Module -Name xNetworking -Scope CurrentUser -Force
+Find-Module xActiveDirectory | Install-Module
+
+#DSC Resource Module Versions
+Get-Module -Name xnetworking -ListAvailable | select Name,Version,ModuleBase
+
+#Finding DSC Resource Syntax & Properties
+Get-DscResource xDNSServerAddress
+Get-DscResource xDNSServerAddress | select -ExpandProperty Properties | ft -AutoSize
+Get-DscResource xDNSServerAddress -Syntax
+
+#Examine DSC Resources
+Start-Process "C:\Program Files (x86)\Microsoft VS Code\Code.exe"
--- DemoScripts/practical-desired-state-configuration-m2-01.ps1
+++ DemoScripts/practical-desired-state-configuration-m2-01.ps1
@@ -1,30 +1,28 @@
-#Discovering DSC Resource Modules
-Find-Module -Includes DscResource
-Find-Module -Tag DSC
-Find-Module -Name *Networking*  -Includes DscResource
+Configuration RenameComputer {
+    param(
+        [string]$NodeName,
+        [string]$NewName
+    )
 
-#Discovering DSC Resources
-Find-DscResource
-Find-DscResource -moduleName xNetworking
-Find-DscResource | Where-Object Name -like *IP*
-Find-DscResource -Name xIPAddress
+    Import-DscResource -ModuleName xComputerManagement 
+            
+    Node $AllNodes.NodeName {         
+    
+    xComputer RenameComputer {
+        Name = $Node.NewName
+    }
+    
+    } #end node block
+    
+} #end configuration
 
+$ConfigData = @{             
+    AllNodes = @(             
+        @{             
+            Nodename = 'WIN-6J22PI2U9RJ'
+            NewName = 'PS-S01'            
+        }                   
+    )             
+}
 
-#Installing DSC Resource Modules
-Install-Module xnet*
-Install-Module -Name xNetworking
-Install-Module -Name xNetworking -Force
-Install-Module -Name xNetworking -Scope CurrentUser -Force
-Find-Module xActiveDirectory | Install-Module
-
-#DSC Resource Module Versions
-Get-Module -Name xnetworking -ListAvailable | select Name,Version,ModuleBase
-
-#Finding DSC Resource Syntax & Properties
-Get-DscResource xDNSServerAddress
-Get-DscResource xDNSServerAddress | select -ExpandProperty Properties | ft -AutoSize
-Get-DscResource xDNSServerAddress -Syntax
-
-#Examine DSC Resources
-Start-Process "C:\Program Files (x86)\Microsoft VS Code\Code.exe"
-
+RenameComputer -ConfigurationData $ConfigData -OutputPath C:\dsc\push
