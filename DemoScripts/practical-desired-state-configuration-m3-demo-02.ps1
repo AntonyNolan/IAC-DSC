12eb9ff2d774d6be9d43d128b298f6692f06defe
Joshua Duffney <duffney@outlook.com>
2016-05-26 15:55:58 -0500

    updated m3 d2 script

--- DemoScripts/practical-desired-state-configuration-m3-demo-02.ps1
+++ DemoScripts/practical-desired-state-configuration-m3-demo-02.ps1
@@ -20,11 +20,11 @@ Copy-Item @params
 Invoke-Command -Session $Session -ScriptBlock {Get-Module xAdcsDeployment -ListAvailable}
 
 #load custom cmdlet into remote session
-. .\New-SelfSignedCertificateEx
+. C:\GitHub\IAC-DSC\Helper-Functions\New-SelfSignedCertificateEx.ps1
+
 Invoke-Command -Session $Session -ScriptBlock {
     New-SelfSignedCertificateEx -Subject 'CN=Cert' -StoreLocation LocalMachine -StoreName My -EnhancedKeyUsage 'Document Encryption' -FriendlyName SelfSigned
 }
-
 
 $cert = Invoke-Command -scriptblock { 
      Get-ChildItem Cert:\LocalMachine\my | 
@@ -32,3 +32,44 @@ $cert = Invoke-Command -scriptblock {
      } -computername cert
 
 Export-Certificate -Cert $cert -FilePath C:\Certs\cert.cer
+
+[DSCLocalConfigurationManager()]
+Configuration LCM_SelfSigned
+{
+    param
+        (
+            [Parameter(Mandatory=$true)]
+            [string[]]$ComputerName,
+
+            [Parameter(Mandatory=$true)]
+            [string]$guid,
+
+            [Parameter(Mandatory=$true)]
+            [string]$thumbprint
+
+        )
+	Node $ComputerName {
+
+		Settings {
+
+			AllowModuleOverwrite = $True
+		    ConfigurationMode = 'ApplyAndAutoCorrect'
+			RefreshMode = 'Push'
+			ConfigurationID = $guid
+            CertificateID = $thumbprint
+            }
+	}
+}
+
+$cim = New-CimSession -ComputerName $Session.ComputerName
+
+$guid=[guid]::NewGuid()
+
+LCM_SelfSigned -ComputerName $Session.ComputerName `
+-Guid $guid -Thumbprint $Cert.Thumbprint -OutputPath c:\DSC\Cert
+
+Set-DscLocalConfigurationManager -CimSession $cim -Path C:\dsc\Cert -Verbose
+
+psEdit C:\GitHub\IAC-DSC\DemoScripts\Configurations\Push\Globomantics_Cert.ps1
+
+Start-DscConfiguration -CimSession $cim -Path c:\dsc\cert -Wait -Force -Verbose
