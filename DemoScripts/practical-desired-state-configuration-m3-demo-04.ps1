112ba6744d89b0601e70927e6e67358e55948c9c
Joshua Duffney <duffney@outlook.com>
2016-05-28 08:28:55 -0500

    md d4

---
+++ DemoScripts/practical-desired-state-configuration-m3-demo-04.ps1
@@ -0,0 +1,4 @@
+﻿#Start Certificate Authority Console mmc
+$mmcPath = "c:\Windows\System32\mmc.exe"
+$mscPath = "c:\Windows\system32\certsrv.msc"
+Start-Process -FilePath $mmcPath -ArgumentList $mscPath
