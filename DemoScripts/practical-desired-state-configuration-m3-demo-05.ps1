47f9bffbf9c36330c05bd7ce8fdf6ac55598315e
Joshua Duffney <duffney@outlook.com>
2016-06-01 20:56:40 -0500

    Initial commit

---
+++ DemoScripts/practical-desired-state-configuration-m3-demo-05.ps1
@@ -0,0 +1,29 @@
+﻿#Create PS Sessions
+$Servers = 's1','s2'
+foreach ($Server in $Servers)
+{
+    $Server = New-PSSession -ComputerName $Server -Name $Server
+}
+Get-PSSession
+
+#Confirm Certificate was Issued
+Invoke-Command -Session $s1,$s2 -ScriptBlock {Get-ChildItem Cert:\LocalMachine\My}
+
+Invoke-Command -Session $s1 -ScriptBlock `
+{Get-ChildItem Cert:\LocalMachine\My | select Thumbprint,Subject,Issuer}
+
+Invoke-Command -Session $s1,$s2 -ScriptBlock `
+{Get-ChildItem Cert:\LocalMachine\My | Where Issuer -eq 'CN=GLOBOMANTICS-CERT-CA, DC=GLOBOMANTICS, DC=COM'}
+
+#Exporting Certificates
+$certs = Invoke-Command -scriptblock { 
+    Get-ChildItem Cert:\LocalMachine\my | 
+    Where-Object {$_.Issuer -eq 'CN=GLOBOMANTICS-CERT-CA, DC=GLOBOMANTICS, DC=COM'}
+    } -Session (Get-PSSession)
+
+$certs
+
+foreach ($Cert in $Certs)
+{
+    Export-Certificate -Cert $Cert -FilePath $env:systemdrive:\Certs\$($Cert.PSComputerName).cer -Force     
+}
