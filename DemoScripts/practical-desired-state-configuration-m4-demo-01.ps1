544d02c5b9c254f488ceeb14ee8c197746b6c3bb
Joshua Duffney <duffney@outlook.com>
2016-06-09 21:42:38 -0500

    commit

---
+++ DemoScripts/practical-desired-state-configuration-m4-demo-01.ps1
@@ -0,0 +1,15 @@
+$PullCert = Invoke-Command -scriptblock { 
+    Get-ChildItem Cert:\LocalMachine\my | 
+    Where-Object {$_.FriendlyName -eq 'PSDSCPullServerCert'}
+    } -ComputerName Cert
+
+$PullCert
+
+Export-Certificate -Cert $PullCert -FilePath "$env:systemdrive:\Certs\PSDSCPullServerCert.cer" -Force
+
+$Session = New-PSSession -ComputerName Pull
+Copy-Item -Path C:\Certs\PSDSCPullServerCert.cer -Destination "C:\" -ToSession $Session
+
+Invoke-Command -ScriptBlock `
+{Import-Certificate -FilePath "$env:systemdrive:\PSDSCPullServerCert.cer" `
+-CertStoreLocation 'Cert:\LocalMachine\My'} ` -Session $Session
